48e24fb9f3e12290e57f4ab8031d93724186dd1b
harshitsinghgaharwar77-art <harshitsinghgaharwar77@gmail.com>
2026-01-09 05:54:21 +0000

    Code challenge submission for question: 129 Sum Root to Leaf Numbers

---
+++ 129 Sum Root to Leaf Numbers/129sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(TreeNode* root, int current) {
+        if (root == NULL)
+            return 0;
+
+        current = current * 10 + root->val;
+
+        // if leaf node
+        if (root->left == NULL && root->right == NULL)
+            return current;
+
+        return solve(root->left, current) + solve(root->right, current);
+    }
+
+    int sumNumbers(TreeNode* root) {
+        return solve(root, 0);
+    }
+};
