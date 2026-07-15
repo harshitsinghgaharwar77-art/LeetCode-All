79e4ac79946a92d9102f564c5d458359652a01b8
harshitsinghgaharwar77-art <harshitsinghgaharwar77@gmail.com>
2026-07-15 16:25:05 +0000

    Code challenge submission for question: 98 Validate Binary Search Tree

---
+++ 98 Validate Binary Search Tree/98validate-binary-search-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    bool solve(TreeNode* root, long long minVal, long long maxVal) {
+        if (root == NULL)
+            return true;
+
+        if (root->val <= minVal || root->val >= maxVal)
+            return false;
+
+        return solve(root->left, minVal, root->val) &&
+               solve(root->right, root->val, maxVal);
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        return solve(root, LLONG_MIN, LLONG_MAX);
+    }
+};
