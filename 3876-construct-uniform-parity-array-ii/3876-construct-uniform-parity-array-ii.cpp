37d8e51ddd176f1ee273925d5e6e497a027a1769
Harshit Singh Gaharwar <harshitsinghgaharwar77@gmail.com>
2026-09-03 06:34:44 +0530

    Time: 0 ms (100%), Space: 166 MB (31.94%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn = *min_element(nums1.begin(), nums1.end());
+        // If minimum is odd, we can make all elements odd
+        if (mn % 2 == 1) {
+            return true;
+        }
+        // If minimum is even, every element must be even
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
